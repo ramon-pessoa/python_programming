921bc397da7edef99ea965a723240ab6ef7d22af
ramonpessoa <ramon@ordigraphe.com>
2019-03-18 19:19:15 -0400

    if i < j

--- solutions_for_coding_problems/verify_if_two_numbers_in_a_list_add_up_to_k.py
+++ solutions_for_coding_problems/verify_if_two_numbers_in_a_list_add_up_to_k.py
@@ -2,14 +2,15 @@ def contains_pair_with_sum(list1, k):
     list1.sort()
     for i, val1 in enumerate(list1):
         j = len(list1) - i - 1
-        val2 = list1[j]
-        sum = val1 + val2
-        if sum < k:
-            i = i + 1
-        elif sum > k:
-            j = j - 1
-        else:
-            return True
+        if i < j:
+            val2 = list1[j]
+            sum = val1 + val2
+            if sum < k:
+                i = i + 1
+            elif sum > k:
+                j = j - 1
+            else:
+                return True
 
     return False
 
