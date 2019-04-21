fa1cc9b81c7e1957798a7ea0dde65e1539e4fcf1
ramonpessoa <ramon@ordigraphe.com>
2019-04-20 21:13:38 -0400

    Solution for coding problems 33: median of a sequence of numbers

--- solutions_for_coding_problems/26-50/print_median.py
+++ solutions_for_coding_problems/26-50/print_median.py
@@ -38,3 +38,53 @@ the standard interface.
 This runs in O(N) space. In terms of time, each new element takes O(log N) time to manipulate the heaps, 
 so this will run in O(N log N) time.
 '''
+
+def get_median(min_heap, max_heap):
+	if len(min_heap) > len(max_heap):
+		return min_heap.find_min()
+	elif len(min_heap) < len(max_heap):
+		return max_heap.find_max()
+	else:
+		min_root = min_heap.find_min()
+		max_root = max_heap.find_max()
+		return (min_root + max_root) / 2
+
+
+def add(num, min_heap, max_heap):
+	# If empty, then just add it to the max heap.
+	if len(min_heap) + len(max_heap) <= 1:
+		max_heap.insert(num)
+		return
+
+	median = get_median(min_heap, max_heap)
+	if num > median:
+		# add it to the min heap
+		min_heap.insert(num)
+	else:
+		max_heap.insert(num)
+
+
+def rebalance(min_heap, max_heap):
+	if len(min_heap) > len(max_heap) + 1:
+		root = min_heap.extract_min()
+		max_heap.insert(root)
+	elif len(max_heap) > len(min_heap) + 1:
+		root = max_heap.extract_max()
+		min_heap.insert(root)
+
+def print_median(min_heap, max_heap):
+	print(get_median(min_heap, max_heap))
+
+
+def running_median(stream):
+	'''
+	Since Python has really terrible support for heaps, 
+	we'll pretend we have some heap objects that have 
+	the standard interface.
+	'''
+	min_heap = minheap()
+	max_heap = maxheap()
+	for num in stream:
+		add(num, min_heap, max_heap)
+		rebalance(min_heap, max_heap)
+		print_median(min_heap, max_heap)
